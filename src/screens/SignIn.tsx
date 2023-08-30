0c6df8f947d67ae4cb7e1c18e6b1a50fa1bfd054
feh-franc0 <fernandofv1110@gmail.com>
2023-08-30 13:46:44 -0300

    Componente Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,23 @@
-import { VStack, Text, Center } from 'native-base'
+import { VStack, Text, Center, Heading } from 'native-base'
 
 import LogoSvg from '@assets/logoT.svg'
 
 export function SignIn() {
   return (
     <VStack flex={1} bg="blue.400">
+
       <Center my={24}>
         <LogoSvg width={200} height={50}/>
 
         <Text color="gray.100" fontSize="sm">
           Cuide com segurança
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
 
     </VStack>
