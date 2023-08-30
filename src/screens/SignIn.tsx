180fba099f883e6b3b573cf9d26dbc4f44a8d1f6
feh-franc0 <fernandofv1110@gmail.com>
2023-08-30 17:15:36 -0300

    Finalizando a SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Text, Center, Heading } from 'native-base'
+import { VStack, Text, Center, Heading, ScrollView } from 'native-base'
 
 import LogoSvg from '@assets/logoT.svg'
 import { Input } from '@components/Input'
@@ -6,36 +6,46 @@ import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="blue.400" px={10}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <VStack flex={1} bg="blue.400" px={10}>
+        <Center my={24}>
+          <LogoSvg width={200} height={50}/>
 
-      <Center my={24}>
-        <LogoSvg width={200} height={50}/>
+          <Text color="gray.100" fontSize="sm">
+            Cuide com segurança
+          </Text>
+        </Center>
 
-        <Text color="gray.100" fontSize="sm">
-          Cuide com segurança
-        </Text>
-      </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse sua conta
+          </Heading>
 
-      <Center>
-        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-          Acesse sua conta
-        </Heading>
+          <Input 
+            placeholder="E-mal" 
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
 
-        <Input 
-          placeholder="E-mal" 
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
+          <Input 
+            placeholder="Senha"
+            secureTextEntry
+          />
 
-        <Input 
-          placeholder="Senha"
-          secureTextEntry
-        />
+          <Button title="Acessar" />
+        </Center>
 
-        <Button title="Acessar" />
-      </Center>
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
 
-        <Button title="Criar conta" variant="outline" />
-    </VStack>
+          <Button 
+            title="Criar conta" 
+            variant="outline" 
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
