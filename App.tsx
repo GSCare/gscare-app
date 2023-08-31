2472c5c66a4741e892448e137dda486d53c99747
feh-franc0 <fernandofv1110@gmail.com>
2023-08-31 16:27:43 -0300

    Removendo o Header

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -17,7 +18,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
