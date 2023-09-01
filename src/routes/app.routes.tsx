cf0f7f6eca47556f5d0d0d9fdb111a1e0a518c16
feh-franc0 <fernandofv1110@gmail.com>
2023-09-01 00:28:45 -0300

    Alterando os ícones da Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,9 @@
+import { useTheme } from "native-base"
 import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
+import HomeSvg from "@assets/home.svg"
+import ClockSvg from "@assets/clock.svg"
+import CalendarSvg from "@assets/calendar.svg"
 
 import { Home } from "@screens/Home";
 import { AttendanceCalendar } from "@screens/AttendanceCalendar";
@@ -33,55 +38,119 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+  const { sizes } = useTheme()
+
+  const iconSize = sizes[7]
+
   return (
     <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
       <Screen 
         name="home" 
         component={Home} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="attendanceCalendar" 
         component={AttendanceCalendar} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="clientsPathologies" 
         component={ClientsPathologies} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="complaintsAndNotices" 
         component={ComplaintsAndNotices} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="dailyReport" 
         component={DailyReport} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="emergencyPhones" 
         component={EmergencyPhones} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="foodAndMedicineAllergies" 
         component={FoodAndMedicineAllergies} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="patientsMedicalDocuments" 
         component={PatientsMedicalDocuments} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="profile" 
         component={Profile} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="settings" 
         component={Settings} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="supplyMonitoring" 
         component={SupplyMonitoring} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
       <Screen 
         name="medicationSchedules" 
         component={MedicationSchedules} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+          )
+        }}
       />
     </Navigator>
   )
