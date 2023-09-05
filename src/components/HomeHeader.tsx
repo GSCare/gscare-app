de4247ac4e1620736d415eee491fd4aabce2b999
feh-franc0 <fernandofv1110@gmail.com>
2023-09-05 16:35:19 -0300

    botao logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,36 @@
-import { HStack, Heading, Text, VStack } from "native-base";
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        size={16} 
+        source={{ uri: 'https://github.com/feh-franc0.png' }}
+        alt="Imagem do usuário ou empresa"
+        mr={4}
+      />
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
-          Rodrigo
+        <Heading color="gray.100" fontSize="md">
+          Home Care
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
