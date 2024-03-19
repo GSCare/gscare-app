878fa4d922e6592224e6717079df582f163752dc
luisfelipeprs <feliperaxz@gmail.com>
2024-03-19 01:06:00 -0300

    passando estilizaçao pra tailwindcss e criando rotas

--- src/components/ModalAddHome.tsx
+++ src/components/ModalAddHome.tsx
@@ -1,23 +1,55 @@
-import React, { useState } from 'react';
-import { Modal, Text, View, Button, TouchableWithoutFeedback, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Modal, Text, View, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
 export const AddModal = ({ visible, onClose, color }: any) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const items = ['Adicionar Intercorrência', 'Adicionar Compromisso', 'Adicionar Lista de Compras']; // Adicione seus componentes aqui
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
 
+  function handleComponent() {
+    if(selectedItem=="Adicionar Intercorrência")navigation.navigate('addIntercurrence')
+    if(selectedItem=="Adicionar Compromisso")navigation.navigate('addAppointment')
+    if(selectedItem=="Adicionar Lista de Compras")navigation.navigate('shoppingList')
+    setIsModalVisible(false);
+  }
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
+
   const handleBackgroundPress = () => {
     setIsModalVisible(false);
   };
 
   const handleModalPress = () => {
-    // Faz nada, apenas nao deixa o modal fechar
+    // Faz nada, apenas não deixa o modal fechar
+  };
+
+  const handleItemPress = (item: string) => {
+    if (selectedItem === item) {
+      setSelectedItem(null);
+    } else {
+      setSelectedItem(item);
+    }
+    console.log('Item escolhido:', item);
+  };
+
+  useEffect(() => {
+    if (!isModalVisible) {
+      setSelectedItem(null); // Limpa a escolha quando o modal fecha
+    }
+  }, [isModalVisible]);
+
+  const handleSave = () => {
+    console.log('Items selecionados:', selectedItem);
+    setIsModalVisible(false);
   };
 
   return (
     <View>
-
       <Modal
         transparent={true}
         visible={isModalVisible}
@@ -26,27 +58,23 @@ export const AddModal = ({ visible, onClose, color }: any) => {
         }}
       >
         <TouchableWithoutFeedback onPress={handleBackgroundPress}>
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+          <View className="flex-1 justify-center items-center bg-[#0000007F]">
             <TouchableWithoutFeedback onPress={handleModalPress}>
-              <View style={{ backgroundColor: "white", padding: 20, borderRadius: 8, width: 300 }}>
-                <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10 }}>Contact Us</Text>
+              <View className="bg-white p-10 justify-center rounded-md w-80 h-auto max-h-[80%]">
 
-                <View style={{ marginBottom: 10 }}>
-                  <Text style={{ fontSize: 16 }}>Name</Text>
-                  <TextInput style={{ borderWidth: 1, borderColor: "#ccc", borderRadius: 4, padding: 8 }} />
-                </View>
-
-                <View style={{ marginBottom: 10 }}>
-                  <Text style={{ fontSize: 16 }}>Email</Text>
-                  <TextInput style={{ borderWidth: 1, borderColor: "#ccc", borderRadius: 4, padding: 8 }} />
-                </View>
-
-                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                  <TouchableOpacity onPress={onClose}>
-                    <Text style={{ color: "blue" }}>Cancel</Text>
+                {items.map((item) => (
+                  <TouchableOpacity key={item} onPress={() => handleItemPress(item)} className="flex-row items-center my-2">
+                    <View className={`w-5 h-5 rounded-full border ${selectedItem === item ? 'bg-blue-500' : 'border-gray-500 '}`}></View>
+                    <Text className="ml-2 text-base">{item}</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={onClose}>
-                    <Text style={{ color: "blue" }}>Save</Text>
+                ))}
+
+                <View className="flex-row justify-between mt-4">
+                  <TouchableOpacity onPress={handleBackgroundPress}>
+                    <Text className="text-blue-500 text-base">Cancelar</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={handleComponent}>
+                    <Text className="text-blue-500 text-base">Avançar</Text>
                   </TouchableOpacity>
                 </View>
               </View>
